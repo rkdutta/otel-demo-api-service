493d813caaf32845bbc04122209b17a3881f6d82
Rajesh Dutta <rdutta@Rajeshs-MacBook-Pro.local>
2022-10-30 16:30:56 +0100

    added custom counter for prometheus = visits_total

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class Controller {
@@ -16,10 +19,13 @@ public class Controller {
 
     private Logger logger = LoggerFactory.getLogger(Controller.class);
 
+    private final MeterRegistry registry;
+
     @Autowired
-    public Controller(CustomerClient customerClient, AddressClient addressClient) {
+    public Controller(CustomerClient customerClient, AddressClient addressClient,MeterRegistry registry) {
         this.customerClient = customerClient;
         this.addressClient = addressClient;
+        this.registry = registry;
     }
 
     @GetMapping(path = "customerDetails/{id}")
@@ -27,6 +33,7 @@ public class Controller {
         logger.info("COLLECTING CUSTOMER AND ADDRESS WITH ID {} FROM UPSTREAM SERVICE", customerId);
         Customer customer = customerClient.getCustomer(customerId);
         Address address = addressClient.getAddressForCustomerId(customerId);
+        registry.counter("visits.total", "name", "hits").increment();
         return new CustomerAndAddress(customer, address);
     }
 
